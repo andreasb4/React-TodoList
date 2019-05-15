f4bdf67b4c85c57335b709227478a29429e001a8
Fanni Boros <topdev999@hotmail.com>
2019-05-15 04:46:24 +0200

    Updates

---
+++ precache-manifest.f028681af1f3c2ab47fa2f88af692133.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ad81203d2a120615095c813d95c6cd9",
+    "url": "/React-TodoList/index.html"
+  },
+  {
+    "revision": "6aa9c08e5148217c7633",
+    "url": "/React-TodoList/static/css/main.46e173c6.chunk.css"
+  },
+  {
+    "revision": "bd0c9146313fb134b61e",
+    "url": "/React-TodoList/static/js/2.7c0a1338.chunk.js"
+  },
+  {
+    "revision": "6aa9c08e5148217c7633",
+    "url": "/React-TodoList/static/js/main.40780d2f.chunk.js"
+  },
+  {
+    "revision": "108dbe416174f98bd927",
+    "url": "/React-TodoList/static/js/runtime~main.9330353c.js"
+  }
+]);
